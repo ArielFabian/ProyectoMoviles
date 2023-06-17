440e5f3433f3fe5aee1dcca046aca64e6b9cdff3
Krozz <juanprdgz99@gmail.com>
2023-06-17 16:09:15 -0600

    vista mitarea

---
+++ app/src/main/java/com/example/proyectomoviles/actitivty_mitarea.java
@@ -0,0 +1,14 @@
+package com.example.proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actitivty_mitarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actitivty_mitarea);
+    }
+}

f1b174496ee2857b23fc81eec1b7cb4f3b5081a1
Krozz <juanprdgz99@gmail.com>
2023-06-17 00:52:22 -0600

    algunos errorsillos y las vistas cambiadas pero aun falla la de crear cuenta no se por que

--- app/src/main/java/com/example/proyectomoviles/activity_crear_cuenta.java
+++ app/src/main/java/com/example/proyectomoviles/activity_crear_cuenta.java
@@ -1,54 +1,14 @@
 package com.example.proyectomoviles;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class activity_crear_cuenta extends AppCompatActivity {
-    public EditText txtNombreCc, txtApellidoCc, txtCorreoCc, txtContrasenaCc;
-    public Button btnCrearCuenta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
-
-        txtNombreCc = (EditText) findViewById(R.id.txtnombrecc);
-        txtApellidoCc = (EditText) findViewById(R.id.txtapellidocc);
-        txtCorreoCc = (EditText) findViewById(R.id.txtcorreocc);
-        txtContrasenaCc = (EditText) findViewById(R.id.txtcontraseñacc);
-        btnCrearCuenta = (Button) findViewById(R.id.btncrearcuenta);
-    }
-
-    public void registrar(View view){
-        String nombre = txtNombreCc.getText().toString();
-        String apellido = txtApellidoCc.getText().toString();
-        String correo = txtCorreoCc.getText().toString();
-        String contrasena = txtContrasenaCc.getText().toString();
-
-        if(!nombre.equals("") && !apellido.equals("") && !correo.equals("") && !contrasena.equals("")){
-            Usuario nuevoUsusario = new Usuario(
-                    nombre + apellido,
-                    correo,
-                    contrasena,
-                    false, // Agregar radios para verificar si es programador o project manager
-                    false,
-                    new ArrayList<Tarea>()
-            );
-
-            //Se inicia la app y se envia el objeto
-            Intent intent = new Intent(this, activity_tareas.class);
-            intent.putExtra("nuevoUsuario", (Serializable) nuevoUsusario);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "No se puede dejar ningun campo vacio, favor de verificar", Toast.LENGTH_LONG).show();
-        }
     }
 }
--- app/src/main/java/com/example/proyectomoviles/activity_perfil.java
+++ app/src/main/java/com/example/proyectomoviles/activity_perfil.java
@@ -1,26 +1,14 @@
 package com.example.proyectomoviles;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class activity_perfil extends AppCompatActivity {
-    public TextView txtNombre, txtCorreo;
-    public Button cerrarSesion;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-
-        txtNombre = (TextView) findViewById(R.id.tvNombre);
-        txtCorreo = (TextView) findViewById(R.id.tvCorreo);
-        cerrarSesion = (Button) findViewById(R.id.btnlogout);
-    }
-    public void cerrarSession(View view){
-
     }
 }
